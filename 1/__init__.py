dc7bbba0b2c930d3d1554c8b9ecb5af8a9f2380d
Phan Thế Tuệ <68698678+phanthetue2309@users.noreply.github.com>
2024-01-31 00:03:29 +0700

    init project

---
+++ 1/__init__.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/two-sum/description/
+from typing import List
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    array_final = []
+    while not array_final:
+        max_number = max(nums)
+        max_number_index = nums.index(max_number)
+        nums[max_number_index] = -100
+        find_value = target - max_number
+        if find_value in nums:
+            find_value_index = nums.index(find_value)
+            array_final = [max_number_index, find_value_index]
+    return sorted(array_final)
+
+
+if __name__ == '__main__':
+    result = twoSum(nums=[2, 7, 11, 15], target=9)
+    print(result)
